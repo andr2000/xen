b3dedf5b8fd4fb42c178780501dfba3d2d6ae016
Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>
2018-02-05 09:55:37 +0200

    sndif: v2 take 0
    Signed-off-by: Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>

---
+++ community/sndif_v2/format_patch.sh
@@ -0,0 +1,1 @@
+git format-patch -2 pr_sdnif_v2_0 -o community/sndif_v2/v0 --cover-letter --subject-prefix="Xen-devel][PATCH"
---
+++ community/sndif_v2/send_patch.sh
@@ -0,0 +1,1 @@
+git send-email --to alsa-devel@alsa-project.org --to xen-devel@lists.xenproject.org --cc=andr2000@gmail.com community/sndif_v2/v0/
