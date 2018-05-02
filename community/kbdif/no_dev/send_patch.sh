194b5f8e924aea0fe8cfb732ba73aea8b5c9d042
Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>
2018-05-02 17:50:14 +0300

    kbdif no dev v4
    Signed-off-by: Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>

--- community/kbdif/no_dev/send_patch.sh
+++ community/kbdif/no_dev/send_patch.sh
@@ -1,1 +1,1 @@
-git send-email --to linux-input@vger.kernel.org --to xen-devel@lists.xenproject.org --to konrad.wilk@oracle.com --to dmitry.torokhov@gmail.com --cc=jandryuk@gmail.com --cc=andr2000@gmail.com community/kbdif/no_dev/v3/
+git send-email --to linux-input@vger.kernel.org --to xen-devel@lists.xenproject.org --to konrad.wilk@oracle.com --to dmitry.torokhov@gmail.com --cc=jandryuk@gmail.com --cc=andr2000@gmail.com community/kbdif/no_dev/v4/
