ab80c82953346d9cfae817a826048ca44a51cbef
Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>
2019-09-30 12:00:50 +0300

    SUPPORT.md
    Signed-off-by: Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>

---
+++ community/support.md/send-patch.sh
@@ -0,0 +1,2 @@
+git send-email --to andrew.cooper3@citrix.com --to George.Dunlap@eu.citrix.com --to ian.jackson@eu.citrix.com --to jbeulich@suse.com --to julien.grall@arm.com --to konrad.wilk@oracle.com --to sstabellini@kernel.org --to wei.liu2@citrix.com --to xen-devel@lists.xenproject.org pv/
+
