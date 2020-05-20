96b9282a48362829153ab0b86eb03e07c096ab0d
Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>
2020-05-20 14:25:19 +0300

    displif v2 EDID offset - v0

---
+++ community/displif_edid_offset/displif-send.sh
@@ -0,0 +1,4 @@
+git send-email --to xen-devel@lists.xenproject.org --to jgross@suse.com \
+  --to ian.jackson@eu.citrix.com --to wei.liu2@citrix.com \
+  --to konrad.wilk@oracle.com \
+  community/displif_edid_offset/v0/
