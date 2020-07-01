5906697ad4921cbd02c0494a9c7b44eaafe98105
Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>
2020-07-01 10:19:48 +0300

    displif v2 EDID offset - v1
    without libgnttab

--- community/displif_edid_offset/displif-send.sh
+++ community/displif_edid_offset/displif-send.sh
@@ -1,4 +1,3 @@
 git send-email --to xen-devel@lists.xenproject.org --to jgross@suse.com \
-  --to ian.jackson@eu.citrix.com --to wei.liu2@citrix.com \
-  --to konrad.wilk@oracle.com \
-  community/displif_edid_offset/v0/
+  --to ian.jackson@eu.citrix.com --to wl@xen.org \
+  community/displif_edid_offset/v1/
