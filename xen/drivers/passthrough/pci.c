04d1e74f3f3bd8da87ee55e6e44874fec398e182
Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>
2020-11-19 10:50:08 +0200

    vpci: add hooks for PCI device assign/de-assign
    When a PCI device gets assigned/de-assigned some work on vPCI side needs
    to be done for that device. Introduce a pair of hooks so vPCI can handle
    that.
    
    Signed-off-by: Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>
    ---
    Since v5:
    - do not split code into run_vpci_init
    - do not check for is_system_domain in vpci_{de}assign_device
    - do not use vpci_remove_device_handlers_locked and re-allocate
      pdev->vpci completely
    - make vpci_deassign_device void
    Since v4:
     - de-assign vPCI from the previous domain on device assignment
     - do not remove handlers in vpci_assign_device as those must not
       exist at that point
    Since v3:
     - remove toolstack roll-back description from the commit message
       as error are to be handled with proper cleanup in Xen itself
     - remove __must_check
     - remove redundant rc check while assigning devices
     - fix redundant CONFIG_HAS_VPCI check for CONFIG_HAS_VPCI_GUEST_SUPPORT
     - use REGISTER_VPCI_INIT machinery to run required steps on device
       init/assign: add run_vpci_init helper
    Since v2:
    - define CONFIG_HAS_VPCI_GUEST_SUPPORT so dead code is not compiled
      for x86
    Since v1:
     - constify struct pci_dev where possible
     - do not open code is_system_domain()
     - extended the commit message

--- xen/drivers/passthrough/pci.c
+++ xen/drivers/passthrough/pci.c
@@ -874,6 +874,8 @@ static int deassign_device(struct domain *d, uint16_t seg, uint8_t bus,
     if ( ret )
         goto out;
 
+    vpci_deassign_device(d, pdev);
+
     if ( pdev->domain == hardware_domain  )
         pdev->quarantine = false;
 
@@ -1429,6 +1431,8 @@ static int assign_device(struct domain *d, u16 seg, u8 bus, u8 devfn, u32 flag)
     ASSERT(pdev && (pdev->domain == hardware_domain ||
                     pdev->domain == dom_io));
 
+    vpci_deassign_device(pdev->domain, pdev);
+
     rc = pdev_msix_assign(d, pdev);
     if ( rc )
         goto done;
@@ -1445,6 +1449,8 @@ static int assign_device(struct domain *d, u16 seg, u8 bus, u8 devfn, u32 flag)
             break;
         rc = hd->platform_ops->assign_device(d, devfn, pci_to_dev(pdev), flag);
     }
+
+    rc = vpci_assign_device(d, pdev);
 
  done:
     if ( rc )
