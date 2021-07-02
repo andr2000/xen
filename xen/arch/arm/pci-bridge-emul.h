97b370e5fd73f01537ede28089659508ffac4b25
Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>
2021-07-02 11:22:36 +0300

    Revert "xen/arm: Adapt PCI bridge emulation code to Xen"
    This reverts commit f7771238b46ba69f9045b4eb8be8b003b03c9ef9.

--- xen/arch/arm/pci-bridge-emul.h
+++ xen/arch/arm/pci-bridge-emul.h
@@ -2,7 +2,7 @@
 #ifndef __PCI_BRIDGE_EMUL_H__
 #define __PCI_BRIDGE_EMUL_H__
 
-#include <xen/kernel.h>
+#include <linux/kernel.h>
 
 /* PCI configuration space of a PCI-to-PCI bridge. */
 struct pci_bridge_emul_conf {
@@ -120,7 +120,7 @@ struct pci_bridge_emul {
 };
 
 enum {
-	PCI_BRIDGE_EMUL_NO_PREFETCHABLE_BAR = BIT(0, U),
+	PCI_BRIDGE_EMUL_NO_PREFETCHABLE_BAR = BIT(0),
 };
 
 int pci_bridge_emul_init(struct pci_bridge_emul *bridge,
