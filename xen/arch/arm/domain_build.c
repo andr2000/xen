d8df4ca5fe488851243efc8492f986fa42e47993
Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>
2021-08-10 16:23:55 +0300

    xen/arm: do not map PCI ECAM and MMIO space to Domain-0's p2m
    PCI host bridges are special devices in terms of implementing PCI
    passthrough. According to [1] the current implementation depends on
    Domain-0 to perform the initialization of the relevant PCI host
    bridge hardware and perform PCI device enumeration. In order to
    achieve that one of the required changes is to not map all the memory
    ranges in map_range_to_domain as we traverse the device tree on startup
    and perform some additional checks if the range needs to be mapped to
    Domain-0.
    
    The generic PCI host controller device tree binding says [2]:
    - ranges: As described in IEEE Std 1275-1994, but must provide
              at least a definition of non-prefetchable memory. One
              or both of prefetchable Memory and IO Space may also
              be provided.
    
    - reg   : The Configuration Space base address and size, as accessed
              from the parent bus.  The base address corresponds to
              the first bus in the "bus-range" property.  If no
              "bus-range" is specified, this will be bus 0 (the default).
    
    From the above none of the memory ranges from the "ranges" property
    needs to be mapped to Domain-0 at startup as MMIO mapping is going to
    be handled dynamically by vPCI as we assign PCI devices, e.g. each
    device assigned to Domain-0/guest will have its MMIOs mapped/unmapped
    as needed by Xen.
    
    The "reg" property covers not only ECAM space, but may also have other
    then the configuration memory ranges described, for example [3]:
    - reg: Should contain rc_dbi, config registers location and length.
    - reg-names: Must include the following entries:
       "rc_dbi": controller configuration registers;
       "config": PCIe configuration space registers.
    
    This patch makes it possible to not map all the ranges from the
    "ranges" property and also ECAM from the "reg". All the rest from the
    "reg" property still needs to be mapped to Domain-0, so the PCI
    host bridge remains functional in Domain-0.
    
    [1] https://lists.xenproject.org/archives/html/xen-devel/2020-07/msg00777.html
    [2] https://www.kernel.org/doc/Documentation/devicetree/bindings/pci/host-generic-pci.txt
    [3] https://www.kernel.org/doc/Documentation/devicetree/bindings/pci/hisilicon-pcie.txt
    
    Signed-off-by: Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>
    ---
    Since v4:
    - update skip_mapping comment
    - add comment why we need to map interrupts to Dom0
    Since v3:
     - pass struct map_range_data to map_dt_irq_to_domain
     - remove redundant check from map_range_to_domain
     - fix handle_device's .skip_mapping
    Since v2:
     - removed check in map_range_to_domain for PCI_DEV
       and moved it to handle_device, so the code is
       simpler
     - s/map_pci_bridge/skip_mapping
     - extended comment in pci_host_bridge_mappings
     - minor code restructure in construct_dom0
     - s/.need_p2m_mapping/.need_p2m_hwdom_mapping and related
       callbacks
     - unsigned int i; in pci_host_bridge_mappings
    Since v1:
     - Added better description of why and what needs to be mapped into
       Domain-0's p2m and what doesn't
     - Do not do any mappings for PCI devices while traversing the DT
     - Walk all the bridges and make required mappings in one go

--- xen/arch/arm/domain_build.c
+++ xen/arch/arm/domain_build.c
@@ -10,7 +10,6 @@
 #include <asm/regs.h>
 #include <xen/errno.h>
 #include <xen/err.h>
-#include <xen/device_tree.h>
 #include <xen/libfdt/libfdt.h>
 #include <xen/guest_access.h>
 #include <xen/iocap.h>
@@ -50,12 +49,6 @@ static int __init parse_dom0_mem(const char *s)
     return *s ? -EINVAL : 0;
 }
 custom_param("dom0_mem", parse_dom0_mem);
-
-struct map_range_data
-{
-    struct domain *d;
-    p2m_type_t p2mt;
-};
 
 /* Override macros from asm/page.h to make them work with mfn_t */
 #undef virt_to_mfn
@@ -1676,10 +1669,11 @@ static int __init map_dt_irq_to_domain(const struct dt_device_node *dev,
                                        const struct dt_irq *dt_irq,
                                        void *data)
 {
-    struct domain *d = data;
+    struct map_range_data *mr_data = data;
+    struct domain *d = mr_data->d;
     unsigned int irq = dt_irq->irq;
     int res;
-    bool need_mapping = !dt_device_for_passthrough(dev);
+    bool need_mapping = !mr_data->skip_mapping;
 
     if ( irq < NR_LOCAL_IRQS )
     {
@@ -1703,13 +1697,12 @@ static int __init map_dt_irq_to_domain(const struct dt_device_node *dev,
     return 0;
 }
 
-static int __init map_range_to_domain(const struct dt_device_node *dev,
-                                      u64 addr, u64 len,
-                                      void *data)
+int __init map_range_to_domain(const struct dt_device_node *dev,
+                               u64 addr, u64 len, void *data)
 {
     struct map_range_data *mr_data = data;
     struct domain *d = mr_data->d;
-    bool need_mapping = !dt_device_for_passthrough(dev);
+    bool need_mapping = !mr_data->skip_mapping;
     int res;
 
     res = iomem_permit_access(d, paddr_to_pfn(addr),
@@ -1752,23 +1745,21 @@ static int __init map_range_to_domain(const struct dt_device_node *dev,
  * then we may need to perform additional mappings in order to make
  * the child resources available to domain 0.
  */
-static int __init map_device_children(struct domain *d,
-                                      const struct dt_device_node *dev,
-                                      p2m_type_t p2mt)
+static int __init map_device_children(const struct dt_device_node *dev,
+                                      struct map_range_data *mr_data)
 {
-    struct map_range_data mr_data = { .d = d, .p2mt = p2mt };
-    int ret;
-
     if ( dt_device_type_is_equal(dev, "pci") )
     {
+        int ret;
+
         dt_dprintk("Mapping children of %s to guest\n",
                    dt_node_full_name(dev));
 
-        ret = dt_for_each_irq_map(dev, &map_dt_irq_to_domain, d);
+        ret = dt_for_each_irq_map(dev, &map_dt_irq_to_domain, mr_data);
         if ( ret < 0 )
             return ret;
 
-        ret = dt_for_each_range(dev, &map_range_to_domain, &mr_data);
+        ret = dt_for_each_range(dev, &map_range_to_domain, mr_data);
         if ( ret < 0 )
             return ret;
     }
@@ -1849,6 +1840,20 @@ static int __init handle_device(struct domain *d, struct dt_device_node *dev,
     int res;
     u64 addr, size;
     bool need_mapping = !dt_device_for_passthrough(dev);
+    /*
+     * For PCI passthrough we only need to remap to Dom0 the interrupts
+     * and memory ranges from "reg" property which cover controller's
+     * configuration registers and such. PCIe configuration space registers
+     * of the PCIe Root Complex and PCIe aperture should not be mapped
+     * automatically to Dom0.
+     */
+    struct map_range_data mr_data = {
+        .d = d,
+        .p2mt = p2mt,
+        .skip_mapping = !need_mapping ||
+                        (is_pci_passthrough_enabled() &&
+                         (device_get_class(dev) == DEVICE_PCI))
+    };
 
     naddr = dt_number_of_address(dev);
 
@@ -1888,7 +1893,6 @@ static int __init handle_device(struct domain *d, struct dt_device_node *dev,
     /* Give permission and map MMIOs */
     for ( i = 0; i < naddr; i++ )
     {
-        struct map_range_data mr_data = { .d = d, .p2mt = p2mt };
         res = dt_device_get_address(dev, i, &addr, &size);
         if ( res )
         {
@@ -1902,7 +1906,7 @@ static int __init handle_device(struct domain *d, struct dt_device_node *dev,
             return res;
     }
 
-    res = map_device_children(d, dev, p2mt);
+    res = map_device_children(dev, &mr_data);
     if ( res )
         return res;
 
@@ -3060,7 +3064,14 @@ static int __init construct_dom0(struct domain *d)
         return rc;
 
     if ( acpi_disabled )
+    {
         rc = prepare_dtb_hwdom(d, &kinfo);
+        if ( rc < 0 )
+            return rc;
+#ifdef CONFIG_HAS_PCI
+        rc = pci_host_bridge_mappings(d, p2m_mmio_direct_c);
+#endif
+    }
     else
         rc = prepare_acpi(d, &kinfo);
 
