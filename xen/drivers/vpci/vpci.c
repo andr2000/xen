aa6511f0107b5ee23f4c16aea21dd5674c063b22
Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>
2021-10-01 14:42:50 +0300

    Fix MSI pdev
    Signed-off-by: Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>

--- xen/drivers/vpci/vpci.c
+++ xen/drivers/vpci/vpci.c
@@ -222,11 +222,11 @@ int vpci_assign_device(struct domain *d, const struct pci_dev *pdev)
     if ( rc )
         goto fail;
 
-    rc = vpci_add_msix_ctrl_hanlder(dev);
+    rc = vpci_add_msix_ctrl_hanlder(pdev);
     if ( rc )
         goto fail;
 
-    rc = vpci_add_msi_ctrl_handler(dev);
+    rc = vpci_add_msi_ctrl_handler(pdev);
     if ( rc )
         goto fail;
 
