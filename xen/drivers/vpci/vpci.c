4bc090cb47b3451f85df22daa341bce770ad1f62
Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>
2021-10-27 12:43:11 +0300

    Fixes: b608d6dd4ba6 ("vpci: Make vpci registers removal a dedicated function")
    ---
    Since v3:
    - remove all R-b's due to changes
    - s/vpci_remove_device_registers/vpci_remove_device_handlers
    - minor comment cleanup
    
    Signed-off-by: Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>

--- xen/drivers/vpci/vpci.c
+++ xen/drivers/vpci/vpci.c
@@ -35,7 +35,7 @@ extern vpci_register_init_t *const __start_vpci_array[];
 extern vpci_register_init_t *const __end_vpci_array[];
 #define NUM_VPCI_INIT (__end_vpci_array - __start_vpci_array)
 
-void vpci_remove_device_registers(const struct pci_dev *pdev)
+void vpci_remove_device_handlers(const struct pci_dev *pdev)
 {
     if ( !has_vpci(pdev->domain) )
         return;
@@ -55,10 +55,8 @@ void vpci_remove_device_registers(const struct pci_dev *pdev)
 
 void vpci_remove_device(struct pci_dev *pdev)
 {
-    struct vcpu *v = current;
-
     vpci_cancel_pending(pdev);
-    vpci_remove_device_registers(pdev);
+    vpci_remove_device_handlers(pdev);
     xfree(pdev->vpci->msix);
     xfree(pdev->vpci->msi);
     xfree(pdev->vpci);
