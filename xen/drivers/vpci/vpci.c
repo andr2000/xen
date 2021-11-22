0f90d89f4adacec4dd8e286e8c5484ce84da46a9
Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>
2021-11-22 11:04:50 +0200

    vpci/header: handle p2m range sets per BAR
    Instead of handling a single range set, that contains all the memory
    regions of all the BARs and ROM, have them per BAR.
    As the range sets are now created when a PCI device is added and destroyed
    when it is removed so make them named and accounted.
    
    Note that rangesets were chosen here despite there being only up to
    3 separate ranges in each set (typically just 1). But rangeset per BAR
    was chosen for the ease of implementation and existing code re-usability.
    
    This is in preparation of making non-identity mappings in p2m for the MMIOs.
    
    Signed-off-by: Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>
    
    ---
    Since v6:
    - update according to the new locking scheme
    - remove odd fail label in modify_bars
    Since v5:
    - fix comments
    - move rangeset allocation to init_bars and only allocate
      for MAPPABLE BARs
    - check for overlap with the already setup BAR ranges
    Since v4:
    - use named range sets for BARs (Jan)
    - changes required by the new locking scheme
    - updated commit message (Jan)
    Since v3:
    - re-work vpci_cancel_pending accordingly to the per-BAR handling
    - s/num_mem_ranges/map_pending and s/uint8_t/bool
    - ASSERT(bar->mem) in modify_bars
    - create and destroy the rangesets on add/remove

--- xen/drivers/vpci/vpci.c
+++ xen/drivers/vpci/vpci.c
@@ -38,6 +38,7 @@ extern vpci_register_init_t *const __end_vpci_array[];
 void vpci_remove_device(struct pci_dev *pdev)
 {
     struct vpci *vpci;
+    unsigned int i;
 
     if ( !has_vpci(pdev->domain) )
         return;
@@ -62,6 +63,10 @@ void vpci_remove_device(struct pci_dev *pdev)
         list_del(&r->node);
         xfree(r);
     }
+
+    for ( i = 0; i < ARRAY_SIZE(vpci->header.bars); i++ )
+        rangeset_destroy(vpci->header.bars[i].mem);
+
     xfree(vpci->msix);
     xfree(vpci->msi);
     xfree(vpci);
