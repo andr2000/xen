98305e3bf10ac3b49d03df017dd36ce66dc2d5a0
Oleksandr Andrushchenko <andr2000@gmail.com>
2025-03-01 13:15:45 +0200

    [EXAMPLE] Changes done by clang-format to the previous commit
    This patch is the result of the:
    
    $ git-clang-format --style=file --verbose HEAD~1
    Running clang-format on the following files:
        tools/xl/xl_info.c
        xen/drivers/char/ns16550.c
    old tree: dceb97b3ef3c80cbb9c29311a7f5783df2f69127
    new tree: a8d36f8f45c3ee0d75e674ecda102ffc6a0a0ef3
    changed files:
        xen/drivers/char/ns16550.c
    
    It is seen that only xen/ part of the patch was touched. The result can
    still be manually adjusted before committing.
    
    Signed-off-by: Oleksandr Andrushchenko <andr2000@gmail.com>

--- xen/drivers/char/ns16550.c
+++ xen/drivers/char/ns16550.c
@@ -340,7 +340,9 @@ static void ns16550_setup_preirq(struct ns16550 *uart)
         if ( divisor )
             uart->baud = uart->clock_hz / (divisor << 4);
         else
-            printk(XENLOG_ERR "Automatic baud rate determination was requested,"  " but a baud rate was not set up\n");
+            printk(
+                XENLOG_ERR
+                "Automatic baud rate determination was requested," " but a baud rate was not set up\n");
     }
     ns_write_reg(uart, UART_LCR, lcr);
 
@@ -348,8 +350,9 @@ static void ns16550_setup_preirq(struct ns16550 *uart)
     ns_write_reg(uart, UART_MCR, UART_MCR_DTR | UART_MCR_RTS);
 
     /* Enable and clear the FIFOs. Set a large trigger threshold. */
-    ns_write_reg(uart, UART_FCR, UART_FCR_ENABLE | UART_FCR_CLRX |
-                 UART_FCR_CLTX | UART_FCR_TRG14);
+    ns_write_reg(uart, UART_FCR,
+                 UART_FCR_ENABLE | UART_FCR_CLRX | UART_FCR_CLTX |
+                     UART_FCR_TRG14);
 }
 
 static void __init cf_check ns16550_init_preirq(struct serial_port *port)
